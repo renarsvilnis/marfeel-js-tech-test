73882151e98975904226984b48f56fb55fdbea6f
Renārs Vilnis <renars.vilnis@gmail.com>
2015-08-10 16:02:23 +0300

    added guides

--- src/js/components/PieChart.js
+++ src/js/components/PieChart.js
@@ -10,13 +10,14 @@ class PieChart {
   constructor(parentNode, data, opts) {
 
     this.data = data;
-    // this.colors = d3.scale.ordinal().range(colors);
-    // this.texts = texts;
-
     this.opts = opts;
 
-    let width = this.opts.width,
+    // minimize lookups
+    let width  = this.opts.width,
         height = this.opts.height;
+
+    this.outerRadius = Math.min(width, height) / 2;
+    this.innerRadius = this.outerRadius - this.opts.circleWidth;
 
     // create svg instance
     this.ctx = d3.select(parentNode)
@@ -25,18 +26,16 @@ class PieChart {
       .attr('height', height)
       .attr('class', 'chart__svg');
     
-    // using a main group to offset drawing to the center off svg
+    // create a main group to offset drawing to the center of svg
+    var translate = 'translate(' + (width / 2) + ', ' + (height / 2) + ')';
     this.mainGroup = this.ctx.append('g')
-      .attr('transform', 'translate(' + (width / 2) + ', ' + (height / 2) + ')');
+      .attr('transform', translate);
     
     // create pie chart constructor
     // TODO: add animation http://bl.ocks.org/mbostock/1346410
     this.pie = d3.layout.pie()
       .sort(null)
       .value(d => d.value);
-    
-    this.outerRadius = Math.min(width, height) / 2;
-    this.innerRadius = this.outerRadius - this.opts.circleWidth;
     
     // pie chart arc constructor
     this.arc = d3.svg.arc()
@@ -62,29 +61,20 @@ class PieChart {
   renderText() {
     this.textGroup = this.mainGroup.append('g');
     
-    this.h1 = this.textGroup
+    let h1 = this.textGroup
       .append('text')
-      .attr('text-anchor', 'middle')
-      .attr('font-weight', '700')
-      .attr('fill', '#999999')
-      .attr('font-family', fontFamily)
-      .attr('font-size', 18)
+      .attr('class', 'chart__title')
       .text(this.data.title);
 
-    this.h2 = this.textGroup
+    let h2 = this.textGroup
       .append('text')
-      .attr('text-anchor', 'middle')
-      .attr('font-weight', '400')
-      .attr('fill', '#303030')
-      .attr('font-family', fontFamily)
-      .attr('font-size', 22)
+      .attr('class', 'chart__sum')
       .text(this.data.sumString);
 
     // verticaly align both items
-    let heightOffset = this.h2.node().getBBox().height / 2;
-    console.log(this.h1.node().getBBox());
-    this.h2.attr('y', heightOffset);
-    this.h1.attr('y', heightOffset - this.h1.node().getBBox().height);
+    let heightOffset = h2.node().getBBox().height / 2;
+    h2.attr('y', heightOffset);
+    h1.attr('y', heightOffset - h1.node().getBBox().height);
   }
   
   renderGuides() {
@@ -101,14 +91,13 @@ class PieChart {
         .attr('class', 'chart__guide-line')
         .attr('x1', 0)
         .attr('x2', 0)
-        .attr('y1', this.outerRadius)
-        .attr('y2', this.outerRadius - this.opts.circleWidth)
+        .attr('y1', this.innerRadius)
+        .attr('y2', this.innerRadius - this.opts.circleWidth / 2)
         .attr('transform', function(d){
           return 'rotate(' + guideRotationScale(d) + ')';
-        })
-        .style('stroke-width', 8)
-        .style('stroke-width', 'round')
-        .style('fill', '#333');
+        });
+        // TODO add specific guide stroke
+        // .style('stroke', '#333');
 
   }
   
